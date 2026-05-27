538b25481d0d67298d08c46089cda6cc284c50ed
adinis <aliadinis18@gmail.com>
2026-05-28 00:41:39 +0100

    init:folder creation

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "@/components/header";
+
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <Header />
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
